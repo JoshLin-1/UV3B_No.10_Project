43a3c557ff081326b71675b3c86c41c62dd5c25c
JoshLin-1 <71364377+JoshLin-1@users.noreply.github.com>
2022-10-16 23:23:03 +0800

    CinematicComplete

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,7 +7,19 @@ public class CameraFollow : MonoBehaviour
 
     [SerializeField] float FollowSpeed = 2f; 
     [SerializeField] float yOffset= 1f; 
+    [SerializeField] float zOffset = -10f; 
     public GameObject target; 
+
+    #if UNITY_EDITOR
+    /// <summary>
+    /// Called when the script is loaded or a value is changed in the
+    /// inspector (Called in the editor only).
+    /// </summary>
+    private void OnValidate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y+ yOffset, zOffset);
+    }   
+    #endif
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +30,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y+ yOffset, -10f);
+       Vector3 newPos = new Vector3(target.transform.position.x, target.transform.position.y+ yOffset, zOffset);
        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed*Time.deltaTime) ;
     }
 }
